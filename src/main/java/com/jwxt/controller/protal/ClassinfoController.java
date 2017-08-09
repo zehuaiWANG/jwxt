c00490aa48ba682f432748f2af416bc0782c65d6
wangzehuai <874697675@qq.com>
2017-08-09 21:47:54 +0800

    page commit

--- src/main/java/com/jwxt/controller/protal/ClassinfoController.java
+++ src/main/java/com/jwxt/controller/protal/ClassinfoController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -32,4 +34,19 @@ public class ClassinfoController {
         return list;
     }
 
+    @RequestMapping(value ="getpageclassinfo.do",method = RequestMethod.POST)
+    @ResponseBody
+    public List<Classinfo> getpageclassinfo(Integer page){
+        List<Classinfo> list = iClassinfoService.findClassinfoList();
+        List<Classinfo> list2 = new ArrayList<Classinfo>();
+        //list2.add(list.get(1));
+        if (list.size()<(page*15)){
+         for (int i = (page-1)*15;i<list.size()-(page-1)*15;i++)
+             list2.add(list.get(i));
+        }else{
+            for (int i = (page-1)*15;i<15*page;i++)
+                list2.add(list.get(i));
+        }
+        return list2;
+    }
 }
